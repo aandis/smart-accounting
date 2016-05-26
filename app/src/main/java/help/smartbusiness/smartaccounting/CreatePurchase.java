3e5659ed1ebb15f36f6a3a601e948a4216c8a5ee
aandis <abhishekrm@iitrpr.ac.in>
2016-05-26 20:45:23 +0530

    Added overall totals to ui.

--- app/src/main/java/help/smartbusiness/smartaccounting/CreatePurchase.java
+++ app/src/main/java/help/smartbusiness/smartaccounting/CreatePurchase.java
@@ -5,27 +5,38 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CreatePurchase extends AppCompatActivity {
 
+    public static final String TAG = CreatePurchase.class.getName();
+
     private TextView purchaseDate;
+    private Map<Integer, MaterialEditText> totalsEditTexts;
+    private MaterialEditText purchaseTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_purchase);
+        totalsEditTexts = new HashMap<>();
+        purchaseTotal = (MaterialEditText) findViewById(R.id.purchase_total);
         setUpDatePicker();
-        setUpAddMorePis();
         setUpDefaultPis();
+        setUpAddMorePis();
     }
 
     private void setUpDatePicker() {
@@ -41,15 +52,34 @@ public class CreatePurchase extends AppCompatActivity {
     private void setUpAddMorePis() {
         final LinearLayout purchaseItemWrapper = (LinearLayout)
                 findViewById(R.id.purchase_item_wrapper);
+
         Button addMorePi = (Button) findViewById(R.id.add_more_pi);
+        // Add listener to add more purchase item views.
         addMorePi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-                LinearLayout layout = (LinearLayout) inflater
+                final LinearLayout layout = (LinearLayout) inflater
                         .inflate(R.layout.purchase_item_layout, purchaseItemWrapper, false);
+
+                // Set layout id to identify this view. This is used when this view
+                // is removed to update the total amount.
+                layout.setId(View.generateViewId());
                 setUpPurchaseItemEditTexts(layout);
                 purchaseItemWrapper.addView(layout);
+
+                ImageButton removePi = (ImageButton) layout.findViewById(R.id.purchase_item_remove);
+                removePi.setVisibility(View.VISIBLE);
+                removePi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Remove this view from the list of edit texts
+                        // and recalculate total amount.
+                        totalsEditTexts.remove(layout.getId());
+                        purchaseItemWrapper.removeView(layout);
+                        updateTotal();
+                    }
+                });
             }
         });
     }
@@ -77,6 +107,7 @@ public class CreatePurchase extends AppCompatActivity {
                 parent.findViewById(R.id.purchase_item_rate);
         MaterialEditText purchaseItemAmount = (MaterialEditText)
                 parent.findViewById(R.id.purchase_item_amount);
+        totalsEditTexts.put(parent.getId(), purchaseItemAmount);
         purchaseItemQuantity.addTextChangedListener(new CustomTextWatcher(
                 purchaseItemQuantity, purchaseItemRate, purchaseItemAmount));
         purchaseItemRate.addTextChangedListener(new CustomTextWatcher(
@@ -104,26 +135,42 @@ public class CreatePurchase extends AppCompatActivity {
             String viewStr = mView.getText().toString();
             String otherStr = mOther.getText().toString();
             if (viewStr.length() > 0 && otherStr.length() > 0) {
-                long viewVal = parseNumber(viewStr);
-                long otherVal = parseNumber(otherStr);
+                float viewVal = parseNumber(viewStr);
+                float otherVal = parseNumber(otherStr);
                 if (viewVal < 0 || otherVal < 0) {
                     mAmount.getText().clear();
                     return;
                 }
-                long amount = viewVal * otherVal;
+                float amount = viewVal * otherVal;
                 mAmount.setText(String.valueOf(amount));
+                updateTotal();
             } else {
                 mAmount.getText().clear();
             }
         }
+    }
 
-        private long parseNumber(String number) {
-            try {
-                return Long.parseLong(number);
-            } catch (NumberFormatException ex) {
-                Toast.makeText(getBaseContext(), "Number too long!", Toast.LENGTH_LONG).show();
-            }
-            return -1;
+
+    private float parseNumber(String number) {
+        try {
+            return Float.parseFloat(number);
+        } catch (NumberFormatException ex) {
+            Toast.makeText(getBaseContext(), "Number too long!", Toast.LENGTH_LONG).show();
         }
+        return -1;
+    }
+
+    private void updateTotal() {
+        float sum = 0;
+        for (MaterialEditText text : totalsEditTexts.values()) {
+            float total = parseNumber(text.getText().toString());
+            if (total < 0) {
+                // One of the edit text has an invalid number which shouldn't happen.
+                Log.d(TAG, "Something isn't right!");
+                return;
+            }
+            sum += total;
+        }
+        purchaseTotal.setText(String.valueOf(sum));
     }
 }
