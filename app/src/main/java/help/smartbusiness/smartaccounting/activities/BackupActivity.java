84d9ec967744961c5e8b1bb66084e3ba4dc3d202
Abhishek <abhishekraj10@outlook.com>
2020-11-23 13:13:43 +0530

    Report signin errors.

--- app/src/main/java/help/smartbusiness/smartaccounting/activities/BackupActivity.java
+++ app/src/main/java/help/smartbusiness/smartaccounting/activities/BackupActivity.java
@@ -2,9 +2,14 @@ package help.smartbusiness.smartaccounting.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import help.smartbusiness.smartaccounting.Utils.AuthHelper;
 import help.smartbusiness.smartaccounting.Utils.GoogleHelper;
@@ -37,24 +42,6 @@ public class BackupActivity extends SmartAccountingActivity {
         finish();
     }
 
-//    @Override
-//    public void onConnectionFailed(@NonNull ConnectionResult result) {
-//        setContentView(R.layout.activity_backup);
-//        if (!result.hasResolution()) {
-//            // show the localized error dialog.
-//            GoogleApiAvailability.getInstance().getErrorDialog(this,
-//                    result.getErrorCode(),
-//                    0).show();
-//            return;
-//        }
-//        try {
-//            result.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);
-//        } catch (IntentSender.SendIntentException e) {
-//            report(e);
-//            Log.e(TAG, "Exception while starting resolution activity", e);
-//        }
-//    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         super.onActivityResult(requestCode, resultCode, resultData);
@@ -72,14 +59,22 @@ public class BackupActivity extends SmartAccountingActivity {
     }
 
     private void handleSignInResult(Intent result) {
-        GoogleSignIn.getSignedInAccountFromIntent(result)
-            .addOnSuccessListener(googleAccount -> {
-                logEvent("app_login", "email", googleAccount.getEmail());
-                AuthHelper.signInUser(this);
-                onLoggedIn();
-            })
-//        .addOnFailureListener() TODO
-        ;
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result);
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            logEvent("app_login", "email", account.getEmail());
+            AuthHelper.signInUser(this);
+            onLoggedIn();
+        } catch (ApiException e) {
+            int code = e.getStatusCode();
+            String errorDescription = GoogleSignInStatusCodes.getStatusCodeString(code);
+            report("signInResult:failed code=" + e.getStatusCode() + " error=" + errorDescription);
+            Log.e(TAG, "signInResult:failed code=" + e.getStatusCode() + " error=" + errorDescription);
+
+            // Let's retry.
+            startActivity(new Intent(this, BackupActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        }
     }
 
     private void logoutUser() {
